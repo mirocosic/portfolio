16663ca24aba6a4ee5174bd0cc1784c665011c48
Miro <mirocosic@gmail.com>
2020-08-15 20:49:41 +0200

    trade comp rewrite

--- src/components/trade/index.js
+++ src/components/trade/index.js
@@ -1,37 +1,31 @@
 import React, { Component } from "react"
-import { View } from "react-native"
-import { DarkModeContext } from "react-native-dark-mode"
+import { View, useColorScheme } from "react-native"
 
 import { Copy } from "../typography"
 import { formatCurrency } from "../../utils/currency"
 
 import styles from "./styles"
 
-class Trade extends Component {
+const Trade = ({ asset, trade }) => {
 
-  static contextType = DarkModeContext
+  const { ticker } = asset
+  const darkMode = useColorScheme() === "dark"
 
-  render() {
-    const { name } = this.props.asset
-    const { trade } = this.props
-    const darkMode = this.context === "dark"
-
-    return (
-      <View style={[styles.container, styles.inlineBetween, darkMode && styles.containerDark]}>
-        <View>
-          <Copy>{name}</Copy>
-          <Copy>Price: {formatCurrency(trade.price)}</Copy>
-          <Copy>Amount: {trade.amount}</Copy>
-          { trade.note && <Copy>Note: { trade.note }</Copy> }
-        </View>
-
-        <View>
-          <Copy>+ 25,14 %</Copy>
-          <Copy>+ 2.17 EUR</Copy>
-        </View>
+  return (
+    <View style={[styles.container, styles.inlineBetween, darkMode && styles.containerDark]}>
+      <View>
+        <Copy>{ticker}</Copy>
+        <Copy>Price: {formatCurrency(trade.price)}</Copy>
+        <Copy>Amount: {trade.amount}</Copy>
+        { trade.note && <Copy>Note: { trade.note }</Copy> }
       </View>
-    )
-  }
+
+      <View>
+        <Copy>+ 25,14 %</Copy>
+        <Copy>+ 2.17 EUR</Copy>
+      </View>
+    </View>
+  )
 
 }
 
