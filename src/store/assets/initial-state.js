d76b4f826245015b95e6a547a0680f8d72c84791
Miro <mirocosic@gmail.com>
2020-08-15 20:48:43 +0200

    added ticket prop to assets

--- src/store/assets/initial-state.js
+++ src/store/assets/initial-state.js
@@ -1,7 +1,7 @@
 export default {
   items: [
-    { id: 1, name: "Bitcoin", icon: "bitcoin" },
-    { id: 2, name: "Ethereum", icon: "money" },
+    { id: 1, name: "Bitcoin", icon: "bitcoin", ticker: "BTC" },
+    { id: 2, name: "Ethereum", icon: "money", ticker: "ETH" },
   ],
   selectedAsset: {},
   assetFilter: false,
