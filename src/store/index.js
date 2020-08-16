d470aa45645b37992103d4bb4fc85e80e8e29046
Miro <mirocosic@gmail.com>
2020-08-16 19:09:29 +0200

    added redux persistance to async storage

--- src/store/index.js
+++ src/store/index.js
@@ -1,44 +1,36 @@
 import { createStore, combineReducers, applyMiddleware } from "redux"
-// import { persistStore, persistReducer } from "redux-persist"
+import { persistStore, persistReducer } from "redux-persist"
+import AsyncStorage from "@react-native-community/async-storage"
 // import storage from "redux-persist/lib/storage"
-// import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2"
+
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2"
 // import createSagaMiddleware from "redux-saga"
 // import { composeWithDevTools } from "redux-devtools-extension"
 
 import common from "./common/reducer"
 import trades from "./trades/reducer"
 import assets from "./assets/reducer"
-// import transactions from "./transactions/reducer"
-// import categories from "./categories/reducer"
-// import accounts from "./accounts/reducer"
-// import labels from "./labels/reducer"
-// import rootSaga from "./sagas"
-
 // const sagaMiddleware = createSagaMiddleware();
 
-// const persistConfig = {
-//   key: "root",
-//   storage,
-//   stateReconciler: autoMergeLevel2, // see "Merge Process" section for details.
-// };
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+  stateReconciler: autoMergeLevel2, // see "Merge Process" section for details.
+};
 
 const reducers = combineReducers({
   common,
   trades,
   assets,
-  //   transactions,
-  //   categories,
-  //   accounts,
-  //   labels,
 })
 
-// const pReducer = persistReducer(persistConfig, reducers)
+const pReducer = persistReducer(persistConfig, reducers)
 
 export const store = createStore(
-  reducers,
+  pReducer,
   // composeWithDevTools(applyMiddleware(sagaMiddleware)),
 )
 // export const store = createStore(reducers, composeWithDevTools(applyMiddleware(sagaMiddleware)))
-// export const persistor = persistStore(store)
+export const persistor = persistStore(store)
 
 // sagaMiddleware.run(rootSaga);
